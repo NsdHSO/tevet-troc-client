192a87bc9cdc2d786722e81cc549124f07a1574e
( Nechiforel David-Samuel ) NsdHSO <nechiforelsamuel@yahoo.com>
2025-03-05 12:48:13 +0200

    chore:🌻 added new button

--- packages/dashboard/src/lib/dashboard/dashboard.component.ts
+++ packages/dashboard/src/lib/dashboard/dashboard.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ButtonComponent } from '../../../../ui/button/src/lib/button/button.component';
+import {
+  Button3Component,
+  Butto3Component,
+} from '../../../../ui/button/src/lib/button/button.component';
 
 @Component({
   selector: 'lib-dashboard',
-  imports: [CommonModule, ButtonComponent],
+  imports: [CommonModule, Butto3Component, Button3Component],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css',
 })
-export default class DashboardComponent {}
+export default class DashboardComponent {
+  pressMe() {
+    console.log('Press me out side dashboard');
+  }
+}
--- packages/ui/button/src/lib/button/button.component.ts
+++ packages/ui/button/src/lib/button/button.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output, output } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -7,4 +7,23 @@ import { CommonModule } from '@angular/common';
   templateUrl: './button.component.html',
   styleUrl: './button.component.css',
 })
-export class ButtonComponent {}
+export class Butto3Component {}
+@Component({
+  selector: 'lib-button3',
+  imports: [CommonModule],
+  template:`
+    <button
+      aria-label="'Iancu'"
+      role="button"
+      (click)="clickButton.emit(true)"
+    >
+      pressMe iancu
+    </button>
+
+  `
+})
+export class Button3Component {
+  @Output()
+  clickButton= new EventEmitter();
+
+}
