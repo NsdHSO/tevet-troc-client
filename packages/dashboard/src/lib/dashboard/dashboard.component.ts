1b3e3c7df80263d346fdd642dd011b57d60abc39
( Nechiforel David-Samuel ) NsdHSO <nechiforelsamuel@yahoo.com>
2025-03-05 12:40:38 +0200

    feat: create button

--- packages/dashboard/src/lib/dashboard/dashboard.component.ts
+++ packages/dashboard/src/lib/dashboard/dashboard.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ButtonComponent } from '../../../../ui/button/src/lib/button/button.component';
 
 @Component({
   selector: 'lib-dashboard',
-  imports: [CommonModule],
+  imports: [CommonModule, ButtonComponent],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css',
 })
