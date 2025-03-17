688321d9dbdb10e4de447430b0ec8711b99a2d93
( Nechiforel David-Samuel ) NsdHSO <nechiforelsamuel@yahoo.com>
2025-03-17 11:47:22 +0200

    chore:🌻 added dotenv

--- packages/dashboard/src/lib/provider/api.token.ts
+++ packages/dashboard/src/lib/provider/api.token.ts
@@ -7,7 +7,10 @@ export const API_CONFIG_DASHBOARD = new InjectionToken('API_CONFIG_DASHBOARD', {
 });
 
 export const API_CONFIG_CARD = new InjectionToken('API_CONFIG_CARD', {
-  factory: () => ({
-    baseUrl:  process.env['TEVET_API']+ '/v1/card',
-  }),
+  factory: () => {
+    const apiUrl = process.env['TEVET_API'] || '';
+    return {
+      baseUrl: apiUrl + '/v1/card',
+    };
+  },
 });
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,16 @@
 const Dotenv = require('dotenv-webpack');
+const webpack = require('webpack');
+
 module.exports = {
-  plugins:[new Dotenv()]
-}
+  plugins: [
+    new Dotenv({
+      systemvars: true,  // Load all system variables from Vercel
+      safe: false,       // Don't require .env file
+      silent: true       // Don't show any errors if .env is missing
+    }),
+    // This explicitly defines the environment variables for Angular
+    new webpack.DefinePlugin({
+      'process.env.TEVET_API': JSON.stringify(process.env.TEVET_API || '')
+    })
+  ]
+};
