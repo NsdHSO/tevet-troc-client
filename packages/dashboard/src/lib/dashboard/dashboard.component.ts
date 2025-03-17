9b99ba2a3fc04ce814e67f747d3026175dac6555
( Nechiforel David-Samuel ) NsdHSO <nechiforelsamuel@yahoo.com>
2025-03-17 11:51:32 +0200

    chore:🌻 added jumbotron

--- packages/dashboard/src/lib/dashboard/dashboard.component.ts
+++ packages/dashboard/src/lib/dashboard/dashboard.component.ts
@@ -2,10 +2,17 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DashboardApiService } from '../service/dashboard-api.service';
 import { LandingCardComponent } from '../components/landing-card/landing-card.component';
+import { JumbotronComponent } from '@tevet-troc-client/jumbotron';
+import { TextComponent } from '@tevet-troc-client/text';
 
 @Component({
   selector: 'lib-dashboard',
-  imports: [CommonModule, LandingCardComponent],
+  imports: [
+    CommonModule,
+    LandingCardComponent,
+    JumbotronComponent,
+    TextComponent,
+  ],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css',
   providers: [DashboardApiService],
