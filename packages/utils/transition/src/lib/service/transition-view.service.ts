fd05d7e6f02816022d83bb8216b717766b86867d
( Nechiforel David-Samuel ) NsdHSO <nechiforelsamuel@yahoo.com>
2025-03-19 07:03:12 +0200

    feat: remove provideIn root and added to app.config remove console.log

--- packages/utils/transition/src/lib/service/transition-view.service.ts
+++ packages/utils/transition/src/lib/service/transition-view.service.ts
@@ -1,9 +1,7 @@
 import { Injectable, signal } from '@angular/core';
 import { ViewTransitionInfo } from '@angular/router';
 
-@Injectable({
-  providedIn: 'root',
-})
+@Injectable()
 export class TransitionViewService {
   currentTransition = signal<ViewTransitionInfo | null>(null);
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,17 @@
-import { ApplicationConfig, importProvidersFrom, inject, provideZoneChangeDetection } from '@angular/core';
+import {
+  ApplicationConfig,
+  importProvidersFrom,
+  inject,
+  provideZoneChangeDetection,
+} from '@angular/core';
 import { FrameWholeModule, RouterConfig } from 'ngx-liburg-frame-side';
 import { IconCoreModule } from 'ngx-liburg-icon';
 import { provideAnimations } from '@angular/platform-browser/animations';
-import { provideRouter, ViewTransitionInfo, withViewTransitions } from '@angular/router';
+import {
+  provideRouter,
+  ViewTransitionInfo,
+  withViewTransitions,
+} from '@angular/router';
 import { appRoutes } from './app.routes';
 import { TransitionViewService } from '@tevet-troc-client/transition';
 
@@ -11,34 +20,33 @@ export const CONFIG_MAIN = Object.freeze({
     {
       path: 'dashboard',
       icon: 'fa_solid:laptop-medical',
-      text: 'Dashboard'
+      text: 'Dashboard',
     },
     {
       path: 'emergency',
       icon: 'fa_solid:2',
-      text: 'Emergency'
-    }
+      text: 'Emergency',
+    },
   ],
   iconApp: 'fa_solid:logo',
-  appConfig: appRoutes
+  appConfig: appRoutes,
 }) as RouterConfig;
 export const appConfig: ApplicationConfig = {
   providers: [
     provideAnimations(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(appRoutes, withViewTransitions({ onViewTransitionCreated })
-    ),
+    provideRouter(appRoutes, withViewTransitions({ onViewTransitionCreated })),
     importProvidersFrom(FrameWholeModule.forRoot(CONFIG_MAIN)),
-    importProvidersFrom(IconCoreModule)
-  ]
+    importProvidersFrom(IconCoreModule),
+    TransitionViewService,
+  ],
 };
 
 function onViewTransitionCreated(info: ViewTransitionInfo): void {
   const viewTransitionService = inject(TransitionViewService);
-  console.log(info);
   viewTransitionService.currentTransition.set(info);
 
   info.transition.finished.finally(() => {
     viewTransitionService.currentTransition.set(null);
-  })
+  });
 }
