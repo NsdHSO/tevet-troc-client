a22ac66a778cda9073c1d4e36a38ede9892801bc
NsdHSO <nechiforelsamuel@yahoo.com>
2025-07-28 07:29:37 +0300

    chore: 🌻 added button in appoitment and add hover cursor state

--- packages/appointment/src/lib/appointment/appointment.component.ts
+++ packages/appointment/src/lib/appointment/appointment.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TextComponent, TextDirective } from '@tevet-troc-client/text';
+import { ButtonComponent } from '@tevet-troc-client/button';
 
 @Component({
   selector: 'lib-appointment',
-  imports: [CommonModule, TextDirective, TextComponent],
+  imports: [CommonModule, TextDirective, TextComponent, ButtonComponent],
   templateUrl: './appointment.component.html',
   styleUrl: './appointment.component.css',
 })
