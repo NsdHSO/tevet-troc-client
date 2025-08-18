3bb07a9f7a2c80fafd0eee424bee7e883692841c
NsdHSO <nechiforelsamuel@yahoo.com>
2025-08-18 06:27:29 +0300

    🎸 added http interceptor

---
+++ packages/utils/http-interceptor/src/lib/http-interceptor/http-interceptor.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'lib-http-interceptor',
+  imports: [CommonModule],
+  templateUrl: './http-interceptor.component.html',
+  styleUrl: './http-interceptor.component.css',
+})
+export class HttpInterceptorComponent {}
