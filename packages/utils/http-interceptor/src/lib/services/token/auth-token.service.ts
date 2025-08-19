fa9fe9ac96897b18ba423402d04d8bd14a6194f8
NsdHSO <nechiforelsamuel@yahoo.com>
2025-08-19 05:15:52 +0300

    docs: 📝 added for auth token

--- packages/utils/http-interceptor/src/lib/services/token/auth-token.service.ts
+++ packages/utils/http-interceptor/src/lib/services/token/auth-token.service.ts
@@ -4,14 +4,21 @@ import { Injectable, signal } from '@angular/core';
 export class AuthTokenService {
   private readonly _accessToken = signal<string | null>(null);
 
+  /* Stored token
+   */
   get accessToken() {
     return this._accessToken();
   }
 
+  /* Setter for token
+   */
   setToken(token: string | null) {
     this._accessToken.set(token);
   }
 
+  /*
+   * Clear token
+   */
   clear() {
     this._accessToken.set(null);
   }
