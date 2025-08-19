95df5dc20c1743f6111f28bb4bd388d4c5519222
NsdHSO <nechiforelsamuel@yahoo.com>
2025-08-19 03:19:17 +0300

    feat: 🎸 added the auth client

--- packages/utils/http-interceptor/src/lib/services/guards/auth.guard.ts
+++ packages/utils/http-interceptor/src/lib/services/guards/auth.guard.ts
@@ -1,11 +1,13 @@
 import { inject } from '@angular/core';
 import { CanMatchFn, Route, UrlSegment } from '@angular/router';
 import { AuthTokenService } from '../token/auth-token.service';
+import { TEVET_API_AUTH_CLIENT } from '../providers/api.token';
 
 export const authCanMatchGuard: CanMatchFn = (
   route: Route,
   segments: UrlSegment[]
 ) => {
+  const authClient = inject(TEVET_API_AUTH_CLIENT);
   const tokenSvc = inject(AuthTokenService);
   const token = tokenSvc.accessToken;
   if (token) return true;
@@ -14,6 +16,6 @@ export const authCanMatchGuard: CanMatchFn = (
   const next = encodeURIComponent(window.location.href);
   const encoded = btoa(next);
 
-  window.location.href = `http://localhost:4300/login?next=${encoded}&client=hospital`;
+  window.location.href = `${authClient.baseUrl}/login?next=${encoded}&client=hospital`;
   return false;
 };
