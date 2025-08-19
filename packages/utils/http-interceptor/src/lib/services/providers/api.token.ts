4816bc72acbf42034433bcb76a4803ea85ada51f
NsdHSO <nechiforelsamuel@yahoo.com>
2025-08-19 05:24:38 +0300

    docs: 📝 added client

--- packages/utils/http-interceptor/src/lib/services/providers/api.token.ts
+++ packages/utils/http-interceptor/src/lib/services/providers/api.token.ts
@@ -6,6 +6,8 @@ export const TEVET_API_AUTH = new InjectionToken('API_AUTH_CONFIG', {
   }),
 });
 
+/* Auth Client Api
+ */
 export const TEVET_API_AUTH_CLIENT = new InjectionToken('TEVET_API_AUTH_CLIENT', {
   factory: () => ({
     baseUrl: process.env['TEVET_API_AUTH_CLIENT'],
