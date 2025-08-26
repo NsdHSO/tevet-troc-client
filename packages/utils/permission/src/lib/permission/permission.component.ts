a4792a959e9b2233ca42728b6f9c771ce81ee6af
NsdHSO <nechiforelsamuel@yahoo.com>
2025-08-26 07:55:27 +0300

    feat: 🎸 added permission and jwt decoded

---
+++ packages/utils/permission/src/lib/permission/permission.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'lib-permission',
+  imports: [CommonModule],
+  templateUrl: './permission.component.html',
+  styleUrl: './permission.component.css',
+})
+export class PermissionComponent {}
