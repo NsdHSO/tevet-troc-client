cfacf607f13a6c2119cf05a49fa2de435340939f
NsdHSO <nechiforelsamuel@yahoo.com>
2025-08-26 17:36:35 +0300

    test: 🐳 dialog and aria description

--- packages/ui/dialog/src/lib/dialog/dialog.component.spec.ts
+++ packages/ui/dialog/src/lib/dialog/dialog.component.spec.ts
@@ -1,5 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { DialogComponent } from './dialog.component';
+import { MAT_DIALOG_DATA, MatDialogModule, MatDialogRef } from '@angular/material/dialog';
 
 describe('DialogComponent', () => {
   let component: DialogComponent;
@@ -7,7 +8,22 @@ describe('DialogComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [DialogComponent],
+      imports: [DialogComponent, MatDialogModule],
+      providers: [
+        {
+          provide: MAT_DIALOG_DATA,
+          useValue: {
+            title: 'Test title',
+            message: 'Test message',
+            showOKBtn: true,
+            showCancelBtn: true,
+          },
+        },
+        {
+          provide: MatDialogRef,
+          useValue: { close: jest.fn() },
+        },
+      ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(DialogComponent);
