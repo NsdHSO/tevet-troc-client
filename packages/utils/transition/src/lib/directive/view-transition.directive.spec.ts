8ba3f839971fe6e797c939a5a7289267863e4eee
NsdHSO <nechiforelsamuel@yahoo.com>
2025-08-26 17:51:29 +0300

    chore: 🌻 remove lint form transition

--- packages/utils/transition/src/lib/directive/view-transition.directive.spec.ts
+++ packages/utils/transition/src/lib/directive/view-transition.directive.spec.ts
@@ -1,3 +1,4 @@
+// eslint-disable-next-file @angular-eslint/template/elements-content
 import { Component } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ViewTransitionDirective,
